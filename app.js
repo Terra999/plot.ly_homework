dc72a5c72b1530457f4dbda58a622f267a757692
Terra Vaughn <vterra28@yahoo.com>
2021-04-20 12:17:57 -0500

    Wrote code to create bar graph.

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ console.log("app.js loaded!");
 
 function DrawBargraph(sampleId) {
   console.log(`DrawBargraph(${sampleId})`);
+
+  d3.json("data/samples.json").then(data => {
+    // console.log(data);
+
+    var samples = data.samples;
+    var resultArray = samples.filter(s => s.id == sampleId);
+    // console.log(resultArray);
+    var result = resultArray[0];
+    // console.log(result);
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+    // console.log(sample_values);
+
+    yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+    var barData = {
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks,
+      type: "bar",
+      text: otu_labels.slice(0, 10).reverse(),
+      orientation: "h"
+    }
+
+    var barArray = [barData];
+
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: {t: 30, 1: 150}
+    }
+
+    Plotly.newPlot("bar", barArray, barLayout)
+  });
 }
 
 function DrawBubblechart(sampleId) {
@@ -41,6 +75,8 @@ function InitDashboard() {
 
     var id = sampleNames[0];
 
+      // Draw the graphs and the metadata
+
     DrawBargraph(id);
     DrawBubblechart(id);
     ShowMetadata(id);
@@ -51,40 +87,6 @@ InitDashboard();
 
 
 
-  // Update the bargraph
-  // Update the bubblechart
-  // Update the demographic information
-
-// Use d3.json() to fetch data from JSON file
-// Incoming data is internally referred to as importedData
-
-
  
-  // Trace1 for the otu_ids data
-  // var trace1 = {
-  //   x: data.map(row => row.otu_ids),
-  //   y: data.map(row => row.otu_labels),
-  //   text: data.map(row => row.otu_labels),
-  //   // name: "Greek",
-  //   type: "bar",
-  //   orientation: "h"
-  // };
-
-  // // data
-  // var chartData = [trace1];
-
-  // // Apply the group bar mode to the layout
-  // var layout = {
-  //   title: "",
-  //   margin: {
-  //     l: 100,
-  //     r: 100,
-  //     t: 100,
-  //     b: 100
-  //   }
-  // };
-
-  // // Render the plot to the div tag with id "plot"
-  // Plotly.newPlot("bar", chartData, layout);
 
   
