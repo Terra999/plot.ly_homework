da23861aa32cbb15d7d8c620d26b0e5c09b2cbdd
Terra Vaughn <vterra28@yahoo.com>
2021-04-21 17:03:37 -0500

    Demographic info is showing up - working on gauge.

--- app.js
+++ app.js
@@ -56,19 +56,14 @@ function DrawBubblechart(sampleId) {
     var sample_values = result.sample_values;
     // console.log(sample_values);
 
-    var desired_max_marker_size = 40;
-    var size = [900, 1075, 1250, 1425, 1601];
-
     var bubbleData = {
       x: otu_ids,
       y: sample_values,
-      // text: ['otu_labels'],
       mode: 'markers',
       marker: {
-        color: ['otu_ids'],
-        size: size,
-        sizeref: 2.0 * Math.max(otu_ids.length)/(desired_max_marker_size **2),
-        sizemode: 'area'
+        color: otu_ids,
+        colorscale: 'Jet',
+        size: sample_values,
       }
  
     };
@@ -99,15 +94,42 @@ function optionChanged(newSampleId) {
 
 function ShowMetadata(sampleId) {
   console.log(`ShowMetadata(${sampleId})`);
-}
+
+  // Update the Demographic Info field
+  var selector = d3.select("#sample-metadata");
+
+  selector.html("");
+
+  d3.json("data/samples.json").then(data => {
+    console.log(data);
+
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(m => m.id == sampleId);
+    // console.log(resultArray);
+    var result = resultArray[0];
+    var wfreq = result.wfreq;
+
+    showGauge(wfreq);
+
+
+    console.log(result);
+
+    // Code help from TA Farshad
+    Object.entries(result).forEach(([key, value]) => {
+      var item = `${key.toUpperCase()}: ${value}`;
+      selector.append("h6").text(item);
+    });
+  });
+
+}   
 function InitDashboard() {
-  console.log("InitDashboard()");
+  // console.log("InitDashboard()");
 
   // Update the dropdown
   var selector = d3.select("#selDataset");
 
   d3.json("data/samples.json").then((importedData) => {
-    console.log(importedData);
+    // console.log(importedData);
       
     var sampleNames = importedData.names;
 
@@ -119,6 +141,7 @@ function InitDashboard() {
     });
 
     var id = sampleNames[0];
+      console.log(id);
 
       // Draw the graphs and the metadata
 
@@ -129,6 +152,52 @@ function InitDashboard() {
 
 }
 InitDashboard();
+
+function showGauge(washingFreq) {
+  // console.log("showGauge");
+  var data = [
+    {
+      type: "indicator",
+      mode: "gauge+number",
+      value: washingFreq,
+      title: { text: "Speed", font: { size: 24 } },
+      delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+      gauge: {
+        axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" },
+        bar: { color: "darkblue" },
+        bgcolor: "white",
+        borderwidth: 2,
+        bordercolor: "gray",
+        steps: [
+          { range: [0, 1], color: "rgba(51, 153, 102, .5)"},
+          { range: [1, 2], rbga: "royalblue" },
+          { range: [2, 3], rbga: "royalblue" },
+          { range: [3, 4], rbga: "royalblue" },
+          { range: [4, 5], color: "royalblue" },
+          { range: [5, 6], color: "royalblue" },
+          { range: [6, 7], color: "royalblue" },
+          { range: [7, 8], color: "royalblue" },
+          { range: [8, 9], color: "royalblue" },
+        ],
+        threshold: {
+          line: { color: "red", width: 4 },
+          thickness: 0.75,
+          value: 490
+        }
+      }
+    }
+  ];
+
+  var layout = {
+    width: 500,
+    height: 400,
+    margin: { t: 25, r: 25, l: 25, b: 25 },
+    paper_bgcolor: "lavender",
+    font: { color: "darkblue", family: "Arial" }
+  };
+
+  Plotly.newPlot('gauge', data, layout);
+}
 
 
 
